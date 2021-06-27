d461c8e1638522db2720f46c4bedcd9293a1bd19
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-26 22:17:31 -0700

    initial commit with dependencies installed

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { render } from "react-dom";
+import React from "react";
+
+const App = () => {
+  return <h1>Here is ye olde App!</h1>;
+};
+
+render(<App />, document.getElementById("app"));
