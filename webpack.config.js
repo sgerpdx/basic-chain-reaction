5c48e5f74d20321adec60e5c742fd373edde6a94
SamuelHGerber <henrylightfoot@gmail.com>
2021-06-28 12:02:02 -0700

    webpack config setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,111 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html",
-});
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+// const htmlPlugin = new HtmlWebPackPlugin({
+//   template: "./src/index.html",
+//   filename: "./index.html",
+// });
+//const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
+//need to learn what exactly this block of code is doing:
+const env = Object.entries({
+  ...require('dotenv').config(),
+  ...process.env,
+}).reduce((acc, [key, value]) => {
+  acc[key] = value;
+  return acc;
+}, {});
+
+// entry and output are set to defaults if not specified
 module.exports = {
-  mode: "development",
+  // entry: './src/index.js',
+  // output: {
+  //   filename: 'bundle.[hash].js',
+  //   path: path.resolve(__dirname, './dist'),
+  //   publicPath: '/',
+  // },
+  devServer: {
+    port: 7888,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.EnvironmentPlugin(env),
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules'],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: ['node_modules'],
+  },
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  require('postcss-import')(),
+                  require('autoprefixer')(),
+                  require('postcss-nested')(),
+                ],
+              },
+            },
+          },
+        ],
       },
+      {
+        test: /\.(jpeg|jpg|png|svg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/1,
+        type: 'asset/resource',
+        use: { loader: 'url-loader' },
+      }
     ],
   },
-  plugins: [htmlPlugin],
+  // plugins: [htmlPlugin],
 };
